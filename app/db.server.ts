498865b31a87fbca85a9de627b41ed9992ebe0af
Logan McAnsh <logan@mcan.sh>
2021-08-18 12:09:22 -0400

    feat: set up prisma
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ app/db.server.ts
@@ -0,0 +1,18 @@
+import { PrismaClient } from "@prisma/client";
+
+let prisma: PrismaClient;
+
+declare global {
+  var db: PrismaClient;
+}
+
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  if (!global.db) {
+    global.db = new PrismaClient();
+  }
+  prisma = global.db;
+}
+
+export { prisma };
