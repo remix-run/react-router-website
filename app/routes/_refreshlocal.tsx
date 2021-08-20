36b431339db875461b9b7ae33a6115c235b892b3
Logan McAnsh <logan@mcan.sh>
2021-08-20 12:55:05 -0400

    feat(refreshlocal): accept ref to download and save to db
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/_refreshlocal.tsx
+++ app/routes/_refreshlocal.tsx
@@ -10,7 +10,6 @@ import {
 import { redirect } from "remix";
 import { coerce } from "semver";
 
-import { GitHubReleaseAPIResponse } from "../@types/github";
 import { prisma } from "../db.server";
 import { findMatchingEntries, getPackage } from "../utils/get-docs.server";
 
@@ -20,158 +19,90 @@ let loader: LoaderFunction = async () => {
 };
 
 let action: ActionFunction = async ({ request, context }) => {
+  const url = new URL(request.url);
+
+  if (process.env.NODE_ENV === "development") {
+    if (!url.port) url.port = "3000";
+  }
+
   let token = request.headers.get("Authorization");
   // verify post request and the token matches
   if (request.method !== "POST") {
     return redirect("/");
   }
 
+  const ref = url.searchParams.get("ref");
+  if (!ref) {
+    return redirect("/sad");
+  }
+
   try {
-    /**
-     * 1. download github data
-     ** 1.a put versions/branches in db
-     ** 1.b { id: versionHeadOrBranch, ref: fullVersionOrBranch }
-     ** 1.c special case the latest version to use "master" branch
-     ** 1.d find semantic "head" like v1 for v1.2.2
-     * 2. fetch tarball for versions not in database
-     ** 2.a use public tarball url
-     * 3. unzip tarball
-     ** 3.a parse frontmatter
-     ** 3.b process markdown with ryan's module
-     * 4. store in db
-     ** 4.a { id: filename + versionHeadOrBranch, filepath, ref, md, html, frontmatter }
-     */
-
-    const releasesPromise = await fetch(
-      `https://api.github.com/repos/remix-run/react-router/releases`
-    );
-
-    const releases = (await releasesPromise.json()) as GitHubReleaseAPIResponse;
-
-    const versionsWeHave = await prisma.version.findMany({
-      select: { fullVersionOrBranch: true },
+    // check if we have this release already
+    let release = await prisma.version.findUnique({
+      where: {
+        fullVersionOrBranch: ref,
+      },
     });
 
-    const justVersion = versionsWeHave.map((v) => v.fullVersionOrBranch);
+    if (release) {
+      return redirect("/sad");
+    }
 
-    const versionsWeNeed = releases.filter(
-      (x) => !justVersion.includes(x.tag_name)
+    const stream = await getPackage(
+      `${context.docs.owner}/${context.docs.repo}`,
+      ref
     );
 
-    console.log(versionsWeNeed.map((v) => v.tag_name));
+    const entries = await findMatchingEntries(stream, "/docs");
 
-    let streams: {
-      stream: any;
-      version: string;
-    }[] = await Promise.all(
-      versionsWeNeed.map(async (version) => {
-        const stream = await getPackage(
-          "remix-run/react-router",
-          `/refs/tags/${version.tag_name}`
-        );
+    let tag = coerce(ref);
 
-        return {
-          stream,
-          version: version.tag_name,
-        };
-      })
-    );
+    if (!tag) {
+      throw new Error("tag provided wasnt valid semver");
+    }
 
-    let allEntries = await Promise.all(
-      streams.map(async (entry) => {
-        const entries = await findMatchingEntries(entry.stream, "/docs");
-        return { entries, version: entry.version };
-      })
-    );
+    let info =
+      tag.major > 0
+        ? `v${tag.major}`
+        : tag.minor > 0
+        ? `v0.${tag.minor}`
+        : `v0.0.${tag.patch}`;
 
-    let versionPromises = [];
-    let docPromises = [];
-
-    for (const entries of allEntries) {
-      let tag = coerce(entries.version);
-      if (!tag) continue;
-      let info =
-        tag.major > 0
-          ? `v${tag.major}`
-          : tag.minor > 0
-          ? `v0.${tag.minor}`
-          : `v0.0.${tag.patch}`;
-
-      versionPromises.push(
-        prisma.version.create({
+    const results = await Promise.allSettled(
+      entries.map((entry) =>
+        prisma.doc.create({
           data: {
-            fullVersionOrBranch: entries.version,
-            versionHeadOrBranch: info,
-          },
-        })
-      );
-
-      docPromises.push(
-        entries.entries.map(async (entry) => {
-          return prisma.doc.create({
-            data: {
-              fileName: entry.path,
-              html: entry.content ?? "wtf",
-              fullVersionOrBranch: {
-                connectOrCreate: {
-                  create: {
-                    fullVersionOrBranch: entries.version,
-                    versionHeadOrBranch: info,
-                  },
-                  where: {
-                    fullVersionOrBranch: entries.version,
-                  },
+            fileName: entry.path,
+            html: entry.content ?? "wtf",
+            fullVersionOrBranch: {
+              connectOrCreate: {
+                create: {
+                  fullVersionOrBranch: ref,
+                  versionHeadOrBranch: info,
+                },
+                where: {
+                  fullVersionOrBranch: ref,
                 },
               },
             },
-          });
+          },
         })
-      );
+      )
+    );
+
+    for (const result of results) {
+      if (result.status === "fulfilled") console.log(result.status);
+      else console.log(result);
     }
 
-    const versionResults = await Promise.allSettled(versionPromises);
-    const docResults = await Promise.allSettled(docPromises);
-
-    for (const result of versionResults) {
-      console.log(result);
-    }
-    for (const result of docResults) {
-      console.log(result);
-    }
-
-    // if (versionsWeNeed.length > 0) {
-    //   let versionPromises: Promise<any>[] = [];
-
-    //   for (const version of versionsWeNeed) {
-    //     let tag = coerce(version.tag_name);
-    //     if (!tag) continue;
-
-    //     let info =
-    //       tag.major > 0
-    //         ? `v${tag.major}`
-    //         : tag.minor > 0
-    //         ? `v0.${tag.minor}`
-    //         : `v0.0.${tag.patch}`;
-
-    //     versionPromises.push(
-    //       prisma.version.create({
-    //         data: {
-    //           fullVersionOrBranch: version.tag_name,
-    //           versionHeadOrBranch: info,
-    //         },
-    //       })
-    //     );
-    //   }
-    // }
-
-    return redirect("/_refreshlocal");
+    return redirect(url.toString());
   } catch (error) {
     console.error(error);
-    return redirect("/_refreshlocal");
+    return redirect(url.toString());
   }
 };
 
-const RefreshAllInstancesDocsPage = () => {
+const RefreshAllInstancesDocsPage: RouteComponent = () => {
   let data = useRouteData();
 
   if (data.notFound) {
