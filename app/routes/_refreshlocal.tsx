be6ff2a1fd5d232723f6beae8c2aa7cb45066717
Logan McAnsh <logan@mcan.sh>
2021-08-20 12:38:41 -0400

    feat: save all docs to db in 1 swoop, causes prisma timeout
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/_refreshlocal.tsx
+++ app/routes/_refreshlocal.tsx
@@ -1,3 +1,5 @@
+import * as React from "react";
+
 import {
   RouteComponent,
   ActionFunction,
@@ -6,17 +8,21 @@ import {
   useRouteData,
 } from "remix";
 import { redirect } from "remix";
+import { coerce } from "semver";
+
+import { GitHubReleaseAPIResponse } from "../@types/github";
 import { prisma } from "../db.server";
-import { findMatchingEntries, getPackage } from "../get-docs.server";
+import { findMatchingEntries, getPackage } from "../utils/get-docs.server";
 
 let loader: LoaderFunction = async () => {
-  return json({ notFound: true }, { status: 404 });
+  // return json({ notFound: true }, { status: 404 });
+  return json({});
 };
 
 let action: ActionFunction = async ({ request, context }) => {
   let token = request.headers.get("Authorization");
   // verify post request and the token matches
-  if (request.method !== "POST" && token !== "some-token") {
+  if (request.method !== "POST") {
     return redirect("/");
   }
 
@@ -36,48 +42,147 @@ let action: ActionFunction = async ({ request, context }) => {
      ** 4.a { id: filename + versionHeadOrBranch, filepath, ref, md, html, frontmatter }
      */
 
-    // let stream = await getPackage("remix-run/react-router", "/refs/tags/v6.0.0-beta.1");
-    let stream = await getPackage("remix-run/react-router", "/refs/heads/dev");
-    let entries = await findMatchingEntries(stream, "/docs");
+    const releasesPromise = await fetch(
+      `https://api.github.com/repos/remix-run/react-router/releases`
+    );
 
-    await Promise.all(
-      entries.map((entry) =>
-        prisma.doc.create({
+    const releases = (await releasesPromise.json()) as GitHubReleaseAPIResponse;
+
+    const versionsWeHave = await prisma.version.findMany({
+      select: { fullVersionOrBranch: true },
+    });
+
+    const justVersion = versionsWeHave.map((v) => v.fullVersionOrBranch);
+
+    const versionsWeNeed = releases.filter(
+      (x) => !justVersion.includes(x.tag_name)
+    );
+
+    console.log(versionsWeNeed.map((v) => v.tag_name));
+
+    let streams: {
+      stream: any;
+      version: string;
+    }[] = await Promise.all(
+      versionsWeNeed.map(async (version) => {
+        const stream = await getPackage(
+          "remix-run/react-router",
+          `/refs/tags/${version.tag_name}`
+        );
+
+        return {
+          stream,
+          version: version.tag_name,
+        };
+      })
+    );
+
+    let allEntries = await Promise.all(
+      streams.map(async (entry) => {
+        const entries = await findMatchingEntries(entry.stream, "/docs");
+        return { entries, version: entry.version };
+      })
+    );
+
+    let versionPromises = [];
+    let docPromises = [];
+
+    for (const entries of allEntries) {
+      let tag = coerce(entries.version);
+      if (!tag) continue;
+      let info =
+        tag.major > 0
+          ? `v${tag.major}`
+          : tag.minor > 0
+          ? `v0.${tag.minor}`
+          : `v0.0.${tag.patch}`;
+
+      versionPromises.push(
+        prisma.version.create({
           data: {
-            html: entry.content ?? "",
-            fileName: entry.path,
-            versionHeadOrBranch: {
-              connectOrCreate: {
-                create: {
-                  fullVersionOrBranch: "dev",
-                  versionHeadOrBranch: "dev",
-                },
-                where: {
-                  versionHeadOrBranch: "dev",
+            fullVersionOrBranch: entries.version,
+            versionHeadOrBranch: info,
+          },
+        })
+      );
+
+      docPromises.push(
+        entries.entries.map(async (entry) => {
+          return prisma.doc.create({
+            data: {
+              fileName: entry.path,
+              html: entry.content ?? "wtf",
+              fullVersionOrBranch: {
+                connectOrCreate: {
+                  create: {
+                    fullVersionOrBranch: entries.version,
+                    versionHeadOrBranch: info,
+                  },
+                  where: {
+                    fullVersionOrBranch: entries.version,
+                  },
                 },
               },
             },
-          },
+          });
         })
-      )
-    );
-    return redirect("/6.0.0-beta.1/faq");
+      );
+    }
+
+    const versionResults = await Promise.allSettled(versionPromises);
+    const docResults = await Promise.allSettled(docPromises);
+
+    for (const result of versionResults) {
+      console.log(result);
+    }
+    for (const result of docResults) {
+      console.log(result);
+    }
+
+    // if (versionsWeNeed.length > 0) {
+    //   let versionPromises: Promise<any>[] = [];
+
+    //   for (const version of versionsWeNeed) {
+    //     let tag = coerce(version.tag_name);
+    //     if (!tag) continue;
+
+    //     let info =
+    //       tag.major > 0
+    //         ? `v${tag.major}`
+    //         : tag.minor > 0
+    //         ? `v0.${tag.minor}`
+    //         : `v0.0.${tag.patch}`;
+
+    //     versionPromises.push(
+    //       prisma.version.create({
+    //         data: {
+    //           fullVersionOrBranch: version.tag_name,
+    //           versionHeadOrBranch: info,
+    //         },
+    //       })
+    //     );
+    //   }
+    // }
+
+    return redirect("/_refreshlocal");
   } catch (error) {
     console.error(error);
-    return redirect("/sad");
+    return redirect("/_refreshlocal");
   }
-
-  return redirect("success");
 };
 
-const RefreshAllInstancesDocsPage: RouteComponent = () => {
+const RefreshAllInstancesDocsPage = () => {
   let data = useRouteData();
 
   if (data.notFound) {
     return <p>404</p>;
   }
 
-  return null;
+  return (
+    <form method="post">
+      <button type="submit">Refresh!</button>
+    </form>
+  );
 };
 
 export default RefreshAllInstancesDocsPage;
---
+++ app/utils/get-fly-instance-urls.server.ts
@@ -0,0 +1,10 @@
+import { Resolver } from "dns/promises";
+
+async function getInstanceURLs(): Promise<string[]> {
+  let resolver = new Resolver();
+  let address = `${process.env.FLY_APP_NAME}.internal`;
+  let ipv6s = await resolver.resolve6(address);
+  return ipv6s.map((ip) => `http://${ip}`);
+}
+
+export { getInstanceURLs };
