f74a03bf4fe852ce5c2baf198d276b9eb15df210
Logan McAnsh <logan@mcan.sh>
2021-08-25 11:13:27 -0400

    fix(remix.config): make lang into a param
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- remix.config.js
+++ remix.config.js
@@ -6,7 +6,7 @@ module.exports = {
   devServerPort: 8002,
   routes(defineRoutes) {
     return defineRoutes((route) => {
-      route("docs/lang", "docs/routes/redirect.tsx");
+      route("docs/:lang", "docs/routes/redirect.tsx");
       route("docs/:lang/:version", "docs/routes/version.tsx", () => {
         route("/", "docs/routes/index.tsx");
         route("*", "docs/routes/splat.tsx");
