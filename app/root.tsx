0ca5dc2176de3e6ab30d9b1b9fa5a9e63bad286a
Logan McAnsh <logan@mcan.sh>
2021-08-25 11:11:18 -0400

    chore: use ico icon
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/root.tsx
+++ app/root.tsx
@@ -21,7 +21,6 @@ function Document({ children }: { children: React.ReactNode }) {
     <html lang="en">
       <head>
         <meta charSet="utf-8" />
-        <link rel="icon" href="/favicon.png" type="image/png" />
         <Meta />
         <Links />
       </head>
