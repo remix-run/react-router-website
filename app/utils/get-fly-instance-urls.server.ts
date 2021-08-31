9c1ea46642d6e9ad26170fec82609df8bdaca785
Logan McAnsh <logan@mcan.sh>
2021-08-31 15:33:06 -0400

    chore: use global
    https://fly.io/docs/reference/privatenetwork/
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/utils/get-fly-instance-urls.server.ts
+++ app/utils/get-fly-instance-urls.server.ts
@@ -2,9 +2,10 @@ import { Resolver } from "dns/promises";
 
 async function getInstanceURLs(): Promise<string[]> {
   let resolver = new Resolver();
-  let address = `${process.env.FLY_APP_NAME}.internal`;
+  let address = `global.${process.env.FLY_APP_NAME}.internal`;
   let ipv6s = await resolver.resolve6(address);
   return ipv6s.map((ip) => `http://${ip}`);
+  return [];
 }
 
 export { getInstanceURLs };
