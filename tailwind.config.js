ce7a28d7bbaba6fbe4bdb3a81d9267b46c915f90
Logan McAnsh <logan@mcan.sh>
2021-09-01 15:35:21 -0400

    chore(tailwind): update default text color and font-size
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,8 +15,10 @@ module.exports = {
       typography: (theme) => ({
         DEFAULT: {
           css: {
-            "h1,h2,h3,h4,h5,h6": {
+            color: "rgba(18, 18, 18, 0.8)",
+            "h1, h2, h3, h4, h5, h6": {
               fontFamily: theme("fontFamily.display").join(),
+              color: theme("colors.black"),
             },
           },
         },
