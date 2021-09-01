fdad1cd3b8a78e127b4516a4239f9eec1a214fc3
Logan McAnsh <logan@mcan.sh>
2021-09-01 15:17:20 -0400

    feat: setup tailwind typogrphy plugin with Founders Grotesk font for headings
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,30 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   mode: "jit",
   purge: ["./app/**/*.{ts,tsx}"],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    fontFamily: {
+      display: ['"Test Founders Grotesk"', ...defaultTheme.fontFamily.sans],
+    },
+    extend: {
+      fontFamily: {
+        sans: ["Inter", ...defaultTheme.fontFamily.sans],
+      },
+      typography: (theme) => ({
+        DEFAULT: {
+          css: {
+            "h1,h2,h3,h4,h5,h6": {
+              fontFamily: theme("fontFamily.display").join(),
+            },
+          },
+        },
+      }),
+    },
   },
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
