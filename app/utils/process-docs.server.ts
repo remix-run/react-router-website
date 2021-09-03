dfc6eb150fd724d4fbafa14b56d66422036ef8d2
Logan McAnsh <logan@mcan.sh>
2021-09-03 18:46:11 -0400

    chore: dont add table of contents to docs

--- app/utils/process-docs.server.ts
+++ app/utils/process-docs.server.ts
@@ -14,7 +14,7 @@ async function processDoc(entry: Entry): Promise<{
 
   let path = entry.path.replace(/^\/docs/, "");
   let title = data.title || path;
-  let html = await processMarkdown(`## toc\n\n${content}`);
+  let html = await processMarkdown(content);
   let langMatch = path.match(/^\/_i18n\/(?<lang>[a-z]{2})\//);
 
   let lang = langMatch?.groups?.lang ?? "en";
