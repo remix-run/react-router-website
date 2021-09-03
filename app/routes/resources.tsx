1876959bd46a22ecc8ac271def1d6b57fd20677c
Logan McAnsh <logan@mcan.sh>
2021-09-03 19:03:52 -0400

    chore: add placeholder /resources page
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ app/routes/resources.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import type { RouteComponent, MetaFunction } from "remix";
+
+const meta: MetaFunction = () => ({
+  title: "React Router | Resources",
+});
+
+const ResourcesPage: RouteComponent = () => {
+  return <h1>TODO</h1>;
+};
+
+export default ResourcesPage;
+export { meta };
