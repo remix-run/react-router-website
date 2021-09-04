04ebc227aed7ba4b13fa02411fbf480353e64268
Logan McAnsh <logan@mcan.sh>
2021-09-04 11:16:31 -0400

    chore(Index): headline changes
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,11 +8,12 @@ const meta: MetaFunction = () => ({
 const IndexPage: RouteComponent = () => {
   return (
     <div className="container py-8">
-      <div className="text-center lg:max-w-[600px] lg:mx-auto">
+      <div className="text-center max-w-[600px] mx-auto">
         <h1 className="text-[40px] leading-[1.2] font-bold text-white lg:text-7xl lg:leading-[0.89]">
-          Learn Once Route Anywhere
+          Learn Once
+          <br /> Route Anywhere
         </h1>
-        <p className="mt-2">
+        <p className="mt-2 text-lg leading-8 lg:text-xl">
           Components are the heart of React's powerful programming model. React
           Router is a collection of navigational components that compose
           declaratively with your application.
