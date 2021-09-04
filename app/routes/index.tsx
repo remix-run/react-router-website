11d9c3bdf0356a19a37a896f234d4364280144a1
Logan McAnsh <logan@mcan.sh>
2021-09-03 20:21:29 -0400

    feat: add initial home page
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

---
+++ app/routes/index.tsx
@@ -0,0 +1,26 @@
+import * as React from "react";
+import type { RouteComponent, MetaFunction } from "remix";
+
+const meta: MetaFunction = () => ({
+  title: "React Router",
+});
+
+const IndexPage: RouteComponent = () => {
+  return (
+    <div className="container py-8">
+      <div className="text-center">
+        <h1 className="text-[40px] leading-[1.2] font-bold text-white lg:text-7xl lg:leading-[0.89] lg:max-w-[600px]">
+          Learn Once Route Anywhere
+        </h1>
+        <p className="mt-2">
+          Components are the heart of React's powerful programming model. React
+          Router is a collection of navigational components that compose
+          declaratively with your application.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default IndexPage;
+export { meta };
