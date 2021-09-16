79e947aa7b6ffe664689967e234ac3f66c99b0de
Chance Strickland <chance.strickland@gmail.com>
2021-09-16 11:41:44 -0700

    resources page styles

--- app/routes/resources.tsx
+++ app/routes/resources.tsx
@@ -1,4 +1,5 @@
 import * as React from "react";
+import cx from "clsx";
 import { ButtonLink } from "../components/button";
 import { Section, Heading } from "../components/section-heading";
 import type { RouteComponent, MetaFunction } from "remix";
@@ -56,217 +57,215 @@ const ResourcesPage: RouteComponent = () => {
         </div>
       </div>
 
-      <div className="resources__links">
-        <div className="container">
-          <Section as="section">
-            <div className="">
-              <Heading className="">Around the Web</Heading>
-              <p className="opacity-80 text-lg leading-8 md:text-xl">
-                Follow the Remix team for the latest updates on our React tools
-                and applications
-              </p>
-            </div>
-            <dl className={``}>
-              {[
-                {
-                  title: "GitHub",
-                  desc: "Join the React Router GitHub repo to get news on the latest updates.",
-                  icon: <IconGithub />,
-                  linkTo: "https://github.com/remix-run/react-router",
-                  linkLabel: "Visit GitHub",
-                },
-                {
-                  title: "Twitter",
-                  desc: "Follow the Remix Twitter account to get regular updates on React Router.",
-                  icon: <IconTwitter />,
-                  linkTo: "https://twitter.com/remix_run",
-                  linkLabel: "Visit Twitter",
-                },
-                {
-                  title: "YouTube",
-                  desc: "Watch tutorials and screencasts from the Remix team.",
-                  icon: <IconYoutube />,
-                  linkTo: "https://www.youtube.com/remix_run",
-                  linkLabel: "Visit YouTube",
-                },
-                {
-                  title: "Stack Overflow",
-                  desc: "Get help from the Remix team on using React Router in your application.",
-                  icon: <IconStackOverflow />,
-                  linkTo: "/",
-                  linkLabel: "Visit Stack Overflow",
-                },
-              ].map(({ icon, title, desc, linkLabel, linkTo }) => {
-                return (
-                  <div key={title}>
-                    <IconBox
-                      icon={icon}
-                      className="bg-[color:var(--base07)] text-[color:var(--base00)]"
-                    />
-                    <dt className="h3">{title}</dt>
-                    <dd>
-                      <div className="opacity-80">{desc}</div>
-                      <div>
-                        <ArrowLink
-                          to={linkTo}
-                          className={`
-                            inline-flex mt-6
-                            items-center
-                            font-semibold
-                            text-[color:var(--base07)]
-                          `}
-                        >
-                          {linkLabel}
-                        </ArrowLink>
-                      </div>
-                    </dd>
-                  </div>
-                );
-              })}
-            </dl>
-          </Section>
-        </div>
-      </div>
-
-      <div className="resources__projects">
-        <div className="container">
-          <Section as="section">
-            <div className="">
-              <Heading className="">Open Source Projects</Heading>
-              <p className="opacity-80 text-lg leading-8 md:text-xl">
-                Apps and libraries from the Remix team to use alongside React
-                Router
-              </p>
-            </div>
-            <Section
-              as="div"
-              className="grid grid-cols-1 grid-flow-row sm:grid-cols-2 lg:grid-cols-3 gap-6"
-            >
-              {[
-                {
-                  imgSrc: "/hero.png",
-                  imgAlt: "TODO",
-                  heading: "Remix",
-                  content: `A new React framework that supports dynamic routing and more.`,
-                  linkTo: "https://remix.run",
-                  linkText: "Visit Remix",
-                },
-                {
-                  imgSrc: "/hero.png",
-                  imgAlt: "TODO",
-                  heading: "Reach UI",
-                  content: `An accessible foundation for React applications and design systems.`,
-                  linkTo: "https://reach.tech",
-                  linkText: "Visit Reach UI",
-                },
-                {
-                  imgSrc: "/hero.png",
-                  imgAlt: "TODO",
-                  heading: "UNPKG",
-                  content: `The CDN for everything on npm, currently serving more than 2 billion requests per day.`,
-                  linkTo: "https://unpkg.com",
-                  linkText: "Visit UNPKG",
-                },
-              ].map((card) => {
-                return (
-                  <Card key={card.heading}>
-                    <CardImage>
-                      <img src={card.imgSrc} alt={card.imgAlt} />
-                    </CardImage>
-                    <CardContent>
-                      <Heading className="mb-2">{card.heading}</Heading>
-                      <p className="opacity-80">{card.content}</p>
-                      <ArrowLink to={card.linkTo} className="mt-4">
-                        {card.linkText}
-                      </ArrowLink>
-                      {/* TODO: Align links to the bottom of cards in each row */}
-                    </CardContent>
-                  </Card>
-                );
-              })}
-            </Section>
-          </Section>
-        </div>
-      </div>
-
-      <div className="resources__versions">
-        <div className="container">
-          <Section as="section">
-            <div className="">
-              <Heading className="">Version Downloads</Heading>
-              <p className="opacity-80 text-lg leading-8 md:text-xl">
-                Free and open-source code for developers to use
-              </p>
-            </div>
-            <Section
-              as="div"
-              className="grid grid-cols-1 grid-flow-row max-w-xl"
-            >
-              {[
-                // TODO: Get versions from releases
-                {
-                  heading: "React Router v6",
-                  date: "September 2020",
-                  docs: "/",
-                  githubLink: "/",
-                },
-                {
-                  heading: "React Router v5",
-                  date: "September 2021",
-                  docs: "/",
-                  githubLink: "/",
-                },
-                {
-                  heading: "React Router v4",
-                  date: "September 2021",
-                  docs: "/",
-                  githubLink: "/",
-                },
-              ].map((release, i, arr) => {
-                return (
-                  <div key={release.heading}>
-                    <div className="flex justify-between items-center mb-4">
-                      <Heading>{release.heading}</Heading>
-                      {i === 0 && (
-                        <div>
-                          <Badge color="green">Latest Release</Badge>
+      <div className="space-y-20 sm:space-y-32 md:space-y-40 lg:space-y-44">
+        <div className="resources__links">
+          <div className="container">
+            <Section as="section">
+              <SectionHeader
+                heading="Around the Web"
+                content={`Follow the Remix team for the latest updates on our React tools
+                and applications`}
+                className="mb-10 md:mb-14"
+              />
+              <dl className="grid grid-cols-1 grid-flow-row sm:grid-cols-2 lg:grid-cols-4 gap-6">
+                {[
+                  {
+                    title: "GitHub",
+                    desc: "Join the React Router GitHub repo to get news on the latest updates.",
+                    icon: <IconGithub />,
+                    linkTo: "https://github.com/remix-run/react-router",
+                    linkLabel: "Visit GitHub",
+                  },
+                  {
+                    title: "Twitter",
+                    desc: "Follow the Remix Twitter account to get regular updates on React Router.",
+                    icon: <IconTwitter />,
+                    linkTo: "https://twitter.com/remix_run",
+                    linkLabel: "Visit Twitter",
+                  },
+                  {
+                    title: "YouTube",
+                    desc: "Watch tutorials and screencasts from the Remix team.",
+                    icon: <IconYoutube />,
+                    linkTo: "https://www.youtube.com/remix_run",
+                    linkLabel: "Visit YouTube",
+                  },
+                  {
+                    title: "Stack Overflow",
+                    desc: "Get help from the Remix team on using React Router in your application.",
+                    icon: <IconStackOverflow />,
+                    linkTo: "/",
+                    linkLabel: "Visit Stack Overflow",
+                  },
+                ].map(({ icon, title, desc, linkLabel, linkTo }) => {
+                  return (
+                    <div key={title}>
+                      <IconBox
+                        icon={icon}
+                        className="bg-[color:var(--base07)] text-[color:var(--base00)] mb-6"
+                      />
+                      <dt className="h3">{title}</dt>
+                      <dd>
+                        <div className="opacity-80">{desc}</div>
+                        <div className="mt-3">
+                          <ArrowLink
+                            to={linkTo}
+                            className="inline-flex items-center font-semibold"
+                          >
+                            {linkLabel}
+                          </ArrowLink>
                         </div>
-                      )}
+                      </dd>
                     </div>
-                    <p className="opacity-80 text-lg leading-8 md:text-xl">
-                      Last Release: {release.date}
-                    </p>
-                    <div className="mt-4">
-                      <ul className="list-none flex items-center">
-                        <li
-                          className={`
+                  );
+                })}
+              </dl>
+            </Section>
+          </div>
+        </div>
+
+        <div className="resources__projects">
+          <div className="container">
+            <Section as="section">
+              <SectionHeader
+                heading="Open Source Projects"
+                content={`Apps and libraries from the Remix team to use alongside React
+              Router`}
+                className="mb-10"
+              />
+              <Section
+                as="div"
+                className="grid grid-cols-1 grid-flow-row sm:grid-cols-2 lg:grid-cols-3 gap-6 xl:gap-8"
+              >
+                {[
+                  {
+                    imgSrc: "https://picsum.photos/500",
+                    imgAlt: "TODO",
+                    heading: "Remix",
+                    content: `A new React framework that supports dynamic routing and more.`,
+                    linkTo: "https://remix.run",
+                    linkText: "Visit Remix",
+                  },
+                  {
+                    imgSrc: "https://picsum.photos/502",
+                    imgAlt: "TODO",
+                    heading: "Reach UI",
+                    content: `An accessible foundation for React applications and design systems.`,
+                    linkTo: "https://reach.tech",
+                    linkText: "Visit Reach UI",
+                  },
+                  {
+                    imgSrc: "https://picsum.photos/504",
+                    imgAlt: "TODO",
+                    heading: "UNPKG",
+                    content: `The CDN for everything on npm, currently serving more than 2 billion requests per day.`,
+                    linkTo: "https://unpkg.com",
+                    linkText: "Visit UNPKG",
+                  },
+                ].map((card) => {
+                  return (
+                    <Card key={card.heading}>
+                      <CardImage>
+                        <img src={card.imgSrc} alt={card.imgAlt} />
+                      </CardImage>
+                      <CardContent>
+                        <Heading className="mb-2">{card.heading}</Heading>
+                        <p className="opacity-80">{card.content}</p>
+                        <ArrowLink to={card.linkTo} className="mt-4">
+                          {card.linkText}
+                        </ArrowLink>
+                      </CardContent>
+                    </Card>
+                  );
+                })}
+              </Section>
+            </Section>
+          </div>
+        </div>
+
+        <div className="resources__versions">
+          <div className="container">
+            <Section as="section">
+              <SectionHeader
+                heading="Version Downloads"
+                content={`Free and open-source code for developers to use`}
+                className="mb-16 md:mb-28"
+              />
+              <Section as="div" className="flex flex-col max-w-xl mx-auto">
+                {[
+                  // TODO: Get versions from releases
+                  {
+                    heading: "React Router v6",
+                    date: "September 2020",
+                    docs: "/",
+                    githubLink: "/",
+                  },
+                  {
+                    heading: "React Router v5",
+                    date: "September 2021",
+                    docs: "/",
+                    githubLink: "/",
+                  },
+                  {
+                    heading: "React Router v4",
+                    date: "September 2021",
+                    docs: "/",
+                    githubLink: "/",
+                  },
+                ].map((release, i, arr) => {
+                  return (
+                    <div
+                      key={release.heading}
+                      className={cx({
+                        "pb-8 md:pb-10 mb-8 md:mb-10 border-b border-[color:var(--base02)]":
+                          i !== arr.length - 1,
+                      })}
+                    >
+                      <div className="flex justify-between items-center mb-4">
+                        <Heading>{release.heading}</Heading>
+                        {i === 0 && (
+                          <div>
+                            <Badge color="green">Latest Release</Badge>
+                          </div>
+                        )}
+                      </div>
+                      <p className="opacity-80 text-lg leading-8 md:text-xl">
+                        Last Release: {release.date}
+                      </p>
+                      <div className="mt-4">
+                        <ul className="list-none flex items-center">
+                          <li
+                            className={`
                             after:content-["•"] after:inline-block after:mx-2 md:after:mx-4 md:after:content-["•"]
                             after:text-blue-500
                           `}
-                        >
-                          <Link
-                            to={release.docs}
-                            aria-label={`Read documentation for ${release.heading}`}
                           >
-                            Read Documentation
-                          </Link>
-                        </li>
-                        <li className="">
-                          <Link
-                            to={release.githubLink}
-                            aria-label={`Visit GitHub release for ${release.heading}`}
-                          >
-                            Visit GitHub
-                          </Link>
-                        </li>
-                      </ul>
+                            <Link
+                              to={release.docs}
+                              aria-label={`Read documentation for ${release.heading}`}
+                            >
+                              Read Documentation
+                            </Link>
+                          </li>
+                          <li className="">
+                            <Link
+                              to={release.githubLink}
+                              aria-label={`Visit GitHub release for ${release.heading}`}
+                            >
+                              Visit GitHub
+                            </Link>
+                          </li>
+                        </ul>
+                      </div>
                     </div>
-                  </div>
-                );
-              })}
+                  );
+                })}
+              </Section>
             </Section>
-          </Section>
+          </div>
         </div>
+
+        <div className="mb-20 sm:mb-32 md:mb-40" />
       </div>
     </div>
   );
@@ -274,3 +273,23 @@ const ResourcesPage: RouteComponent = () => {
 
 export default ResourcesPage;
 export { meta };
+
+function SectionHeader({
+  heading,
+  children,
+  content,
+  className,
+}: {
+  heading: React.ReactNode;
+  content: React.ReactNode;
+  children?: React.ReactNode;
+  className?: string;
+}) {
+  return (
+    <div className={cx(className, "text-center m-auto max-w-3xl")}>
+      <Heading className="">{heading}</Heading>
+      <p className="opacity-80 text-lg leading-8 md:text-xl">{content}</p>
+      {children}
+    </div>
+  );
+}
