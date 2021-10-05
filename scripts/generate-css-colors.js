e4150298319f1d83e8101986b09807e8ed8beccd
Chance Strickland <chance.strickland@gmail.com>
2021-10-04 18:48:20 -0700

    update color script

--- scripts/generate-css-colors.js
+++ scripts/generate-css-colors.js
@@ -1,32 +1,46 @@
 const colors = require("../app/utils/colors");
 const path = require("path");
-const fs = require("fs");
+const { writeFile } = require("fs/promises");
 
-let css = "/* THIS FILE IS GENERATED. DO NOT EDIT DIRECTLY! */\n";
-css += "/* @see app/utils/colors.js */\n";
-css += ":root {";
-for (let color in colors) {
-  if (typeof colors[color] === "string") {
-    css += `\n  --color-${color}: ${colors[color]};`;
-  } else {
-    for (let variant in colors[color]) {
-      if (variant === "DEFAULT") {
-        css += `\n  --color-${color}: ${colors[color][variant]};`;
-      } else {
-        css += `\n  --color-${color}-${variant}: ${colors[color][variant]};`;
+main();
+
+async function main() {
+  try {
+    await writeFile(path.resolve(__dirname, "../styles/colors.css"), getCss(), {
+      encoding: "utf8",
+    });
+    console.log("\x1b[32m", "✅ CSS file `colors.css` successfully written!");
+  } catch (err) {
+    console.error("🙅‍♀️ There was an error writing the CSS file.");
+    console.log("\n  ----------------------------------------------\n\n");
+    console.error(err);
+  }
+}
+
+function getCss() {
+  let css = "/* THIS FILE IS GENERATED. DO NOT EDIT DIRECTLY! */\n";
+  css += "/* @see app/utils/colors.js */\n";
+  css += ":root {";
+  for (let [color, definitions] of Object.entries(colors)) {
+    if (typeof definitions === "string") {
+      css += `\n  --color-${color}: ${definitions};`;
+    } else {
+      for (let [variant, value] of Object.entries(definitions).sort(
+        sortColorEntries
+      )) {
+        if (variant === "DEFAULT") {
+          css += `\n  --color-${color}: ${value};`;
+        } else {
+          css += `\n  --color-${color}-${variant}: ${value};`;
+        }
       }
     }
   }
+  return css + "\n}\n";
 }
-css += "\n}\n";
 
-try {
-  fs.writeFileSync(path.resolve(__dirname, "../styles/colors.css"), css, {
-    encoding: "utf8",
-  });
-  console.log("\x1b[32m", "✅ CSS file `colors.css` successfully written!");
-} catch (err) {
-  console.error("🙅‍♀️ There was an error writing the CSS file.");
-  console.log("\n  ----------------------------------------------\n\n");
-  console.error(err);
+function sortColorEntries([a], [b]) {
+  if (a === "DEFAULT") return 1;
+  if (b === "DEFAULT") return -1;
+  return a > b ? 1 : a < b ? -1 : 0;
 }
