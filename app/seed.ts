29429c311d026464b5966d97cba51b9f6d4c87ba
Logan McAnsh <logan@mcan.sh>
2021-10-15 14:46:41 -0400

    chore(ci): remove beta 5 from seed
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/seed.ts
+++ app/seed.ts
@@ -4,7 +4,6 @@ installGlobals();
 
 async function seed() {
   await Promise.all([
-    saveDocs("/refs/tags/v6.0.0-beta.5", ""),
     saveDocs("/refs/tags/v6.0.0-beta.6", ""),
     saveDocs("/refs/heads/main", ""),
   ]);
