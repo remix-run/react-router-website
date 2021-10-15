862245b2b9f80eee6120326b6f0d13d3e8dc110f
Logan McAnsh <logan@mcan.sh>
2021-10-15 16:45:21 -0400

    chore(_refs): show createdAt and updatedAt
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/routes/_refs.tsx
+++ app/routes/_refs.tsx
@@ -1,23 +1,42 @@
+import { Prisma } from "@prisma/client";
 import {
   RouteComponent,
   LoaderFunction,
   MetaFunction,
   useLoaderData,
+  Link,
 } from "remix";
 import { json } from "remix";
 import { prisma } from "~/db.server";
 
+const refs = Prisma.validator<Prisma.GitHubRefArgs>()({
+  select: {
+    ref: true,
+    createdAt: true,
+    updatedAt: true,
+  },
+});
+
+type Ref = Prisma.GitHubRefGetPayload<typeof refs>;
+
 interface RouteData {
-  refs: string[];
+  refs: (Ref & { versionOrBranch: string })[];
 }
 
-const loader: LoaderFunction = async ({ request }) => {
+const loader: LoaderFunction = async () => {
   const refs = await prisma.gitHubRef.findMany({
-    select: { ref: true },
+    select: {
+      ref: true,
+      createdAt: true,
+      updatedAt: true,
+    },
   });
 
   return json({
-    refs: refs.map(({ ref }) => ref),
+    refs: refs.map((ref) => ({
+      ...ref,
+      versionOrBranch: ref.ref.replace(/^refs\/(heads|tags)\//, ""),
+    })),
   });
 };
 
@@ -28,9 +47,19 @@ const meta: MetaFunction = () => ({
 const RefsPage: RouteComponent = () => {
   const data = useLoaderData<RouteData>();
   return (
-    <div>
+    <div className="px-6">
       <h1>Refs we got</h1>
-      <pre>{JSON.stringify(data, null, 2)}</pre>
+      <ul className="space-y-10">
+        {data.refs.map((ref) => (
+          <li key={ref.ref}>
+            <div>
+              <Link to={`/docs/en/${ref.versionOrBranch}`}>{ref.ref}</Link>
+              <p>createdAt: {ref.createdAt}</p>
+              <p>updatedAt: {ref.updatedAt}</p>
+            </div>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
