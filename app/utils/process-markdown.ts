55876e3e291e4bfbd4e466bd4a776baac05ff885
Logan McAnsh <logan@mcan.sh>
2021-10-21 10:41:32 -0400

    chore: update SITE_URL check
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/utils/process-markdown.ts
+++ app/utils/process-markdown.ts
@@ -1,18 +1,14 @@
 import { processMarkdown, remarkCodeBlocksShiki } from "@ryanflorence/md";
 
-if (!process.env.SITE_URL) {
+let baseUrl = process.env.SITE_URL;
+
+if (!baseUrl) {
   throw Error(
     `The SITE_URL environment is not defined for the ${process.env.NODE_ENV} environment.`
   );
 }
 
-let baseUrl: URL;
-try {
-  if (process.env.SITE_URL.endsWith("/")) {
-    throw Error();
-  }
-  baseUrl = new URL(process.env.SITE_URL);
-} catch (err) {
+if (baseUrl.endsWith("/")) {
   throw Error(
     `Invalid SITE_URL environment defined for the ${process.env.NODE_ENV} environment. Ensure that its value is a valid URL without a trailing slash.`
   );
