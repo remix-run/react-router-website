068861fb0b53f03c9f24cc66a03435c631c37789
Logan McAnsh <logan@mcan.sh>
2021-10-21 10:41:38 -0400

    chore: add SITE_URL to global.d.ts
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/@types/global.d.ts
+++ app/@types/global.d.ts
@@ -1,12 +1,13 @@
 declare namespace NodeJS {
   interface ProcessEnv {
-    FLY_APP_NAME: string;
-    AUTH_TOKEN: string;
-    DATABASE_URL: string;
-    LOCAL_DOCS_PATH: string;
-    REPO: string;
-    REPO_DOCS_PATH: string;
-    REPO_LATEST_BRANCH: string;
-    CONVERTKIT_KEY: string;
+    readonly FLY_APP_NAME: string;
+    readonly AUTH_TOKEN: string | undefined;
+    readonly DATABASE_URL: string | undefined;
+    readonly LOCAL_DOCS_PATH: string | undefined;
+    readonly REPO: string | undefined;
+    readonly REPO_DOCS_PATH: string | undefined;
+    readonly REPO_LATEST_BRANCH: string | undefined;
+    readonly CONVERTKIT_KEY: string | undefined;
+    readonly SITE_URL: string | undefined;
   }
 }
