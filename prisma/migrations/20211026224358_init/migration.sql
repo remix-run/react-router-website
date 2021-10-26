b768b4eb6a72fda5316ac719d1315dba3495c1a9
Logan McAnsh <logan@mcan.sh>
2021-10-26 18:46:14 -0400

    chore: migrate to postgres
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- prisma/migrations/20211026224358_init/migration.sql
+++ prisma/migrations/20211026224358_init/migration.sql
@@ -1,17 +1,23 @@
 -- CreateTable
 CREATE TABLE "GitHubRef" (
-    "ref" TEXT NOT NULL PRIMARY KEY,
-    "releaseNotes" TEXT NOT NULL
+    "ref" TEXT NOT NULL,
+    "releaseNotes" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "GitHubRef_pkey" PRIMARY KEY ("ref")
 );
 
 -- CreateTable
 CREATE TABLE "Doc" (
-    "id" TEXT NOT NULL PRIMARY KEY,
+    "id" TEXT NOT NULL,
     "filePath" TEXT NOT NULL,
     "md" TEXT NOT NULL,
     "html" TEXT NOT NULL,
     "lang" TEXT NOT NULL,
     "hasContent" BOOLEAN NOT NULL DEFAULT false,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
     "title" TEXT NOT NULL,
     "order" INTEGER,
     "disabled" BOOLEAN NOT NULL DEFAULT false,
@@ -21,8 +27,12 @@ CREATE TABLE "Doc" (
     "hidden" BOOLEAN NOT NULL DEFAULT false,
     "toc" BOOLEAN NOT NULL DEFAULT true,
     "githubRefId" TEXT NOT NULL,
-    CONSTRAINT "Doc_githubRefId_fkey" FOREIGN KEY ("githubRefId") REFERENCES "GitHubRef" ("ref") ON DELETE CASCADE ON UPDATE CASCADE
+
+    CONSTRAINT "Doc_pkey" PRIMARY KEY ("id")
 );
 
 -- CreateIndex
 CREATE UNIQUE INDEX "Doc_filePath_githubRefId_lang_key" ON "Doc"("filePath", "githubRefId", "lang");
+
+-- AddForeignKey
+ALTER TABLE "Doc" ADD CONSTRAINT "Doc_githubRefId_fkey" FOREIGN KEY ("githubRefId") REFERENCES "GitHubRef"("ref") ON DELETE CASCADE ON UPDATE CASCADE;
