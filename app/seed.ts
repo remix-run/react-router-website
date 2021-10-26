351d9134fbf98920c62fb8ddf7ffb72cdae984d9
Logan McAnsh <logan@mcan.sh>
2021-10-26 17:33:11 -0400

    chore(seed): seed via semver range
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- app/seed.ts
+++ app/seed.ts
@@ -1,12 +1,33 @@
 import { saveDocs } from "./utils/save-docs";
 import { installGlobals } from "@remix-run/node";
+import type { GitHubRelease } from "./@types/github";
+import { satisfies } from "semver";
+
 installGlobals();
 
 async function seed() {
-  await Promise.all([
-    saveDocs("refs/tags/v6.0.0-beta.7", ""),
-    saveDocs("refs/heads/main", ""),
-  ]);
+  let releasesPromise = await fetch(
+    `https://api.github.com/repos/${process.env.REPO}/releases`,
+    {
+      headers: {
+        accept: "application/vnd.github.v3+json",
+      },
+    }
+  );
+
+  let releases = (await releasesPromise.json()) as GitHubRelease[];
+
+  const releasesToUse = releases.filter((release: any) => {
+    return satisfies(release.tag_name, ">=6.0.0-beta.8");
+  });
+
+  let promises: Promise<void>[] = [saveDocs("refs/heads/main", "")];
+
+  for (let release of releasesToUse) {
+    promises.push(saveDocs(`/refs/tags/${release.tag_name}`, release.body));
+  }
+
+  await Promise.all(promises);
 }
 
 try {
