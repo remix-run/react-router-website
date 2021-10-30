967912f909d99a98fef8f064fcf597b893ab4148
Ryan Florence <rpflorence@gmail.com>
2021-10-30 07:44:45 -0600

    Added CSP

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -16,6 +16,10 @@ export default function handleRequest(
 
   responseHeaders.set("Content-Type", "text/html");
   responseHeaders.set("X-Powered-By", "Remix");
+  responseHeaders.set(
+    "Content-Security-Policy",
+    "default-src 'self'; img-src *;"
+  );
   return new Response(streamString`<!DOCTYPE html>${markup}`, {
     status: responseStatusCode,
     headers: responseHeaders,
