ef4dcd991d0628bf3e5459b104181bd0647300fd
Ryan Florence <rpflorence@gmail.com>
2022-05-24 00:56:32 -0600

    only seed a couple

--- app/seed.ts
+++ app/seed.ts
@@ -7,32 +7,19 @@ import invariant from "tiny-invariant";
 installGlobals();
 
 async function seed() {
-  let releasesPromise = await fetch(
-    `https://api.github.com/repos/${process.env.REPO}/releases`,
-    {
-      headers: {
-        accept: "application/vnd.github.v3+json",
-      },
-    }
-  );
+  // let releasesPromise = await fetch(
+  //   `https://api.github.com/repos/${process.env.REPO}/releases`,
+  //   {
+  //     headers: {
+  //       accept: "application/vnd.github.v3+json",
+  //     },
+  //   }
+  // );
 
-  let releases = (await releasesPromise.json()) as GitHubRelease[];
-
-  let sorted = releases.sort((a, b) => rcompare(a.tag_name, b.tag_name));
-
-  let latest = sorted.at(0);
-
-  invariant(latest, "Could not determine latest release");
-
-  const releasesToUse = releases.filter((release) => {
-    return satisfies(release.tag_name, `>=${latest!.tag_name}`);
-  });
-
-  let promises: Promise<void>[] = [saveDocs("refs/heads/main", "")];
-
-  for (let release of releasesToUse) {
-    promises.push(saveDocs(`refs/tags/${release.tag_name}`, release.body));
-  }
+  let promises: Promise<void>[] = [
+    saveDocs("refs/heads/main", ""),
+    saveDocs("refs/heads/v6.3.0", "")
+  ];
 
   await Promise.all(promises);
 }
