ad4380dc8754868e335da5dc151142018b753034
Ryan Florence <rpflorence@gmail.com>
2022-06-10 15:08:45 -0600

    go straight to the main branch

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,6 @@
 import type { LoaderFunction } from "@remix-run/node";
 import { redirect } from "@remix-run/node";
-import { getLatestRepoTag } from "~/modules/gh-docs";
 
 export let loader: LoaderFunction = async () => {
-  let tag = await getLatestRepoTag();
-
-  if (!tag) {
-    throw new Response("Failed to fetch tags from GitHub", { status: 503 });
-  }
-
-  return redirect(`/en/${tag}`);
+  return redirect(`/en/main`);
 };
