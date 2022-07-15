5cbb9aeaf849c86b80a671fef06fe0f2043a4530
Ryan Florence <rpflorence@gmail.com>
2022-07-15 08:26:35 -0600

    need the idle callback?

--- app/entry.client.tsx
+++ app/entry.client.tsx
@@ -2,16 +2,11 @@ import * as React from "react";
 import { RemixBrowser } from "@remix-run/react";
 import { hydrateRoot } from "react-dom/client";
 
-// enable :active styles on iOS since we disabled the gross tap highlight color
-document.addEventListener("touchstart", function () {}, true);
-
-requestIdleCallback(() => {
-  React.startTransition(() => {
-    hydrateRoot(
-      document,
-      <React.StrictMode>
-        <RemixBrowser />
-      </React.StrictMode>
-    );
-  });
+React.startTransition(() => {
+  hydrateRoot(
+    document,
+    <React.StrictMode>
+      <RemixBrowser />
+    </React.StrictMode>
+  );
 });
