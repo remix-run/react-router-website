c87b2a7755477a67215e7194e234cb4b6d86b683
Ryan Florence <rpflorence@gmail.com>
2022-12-08 14:02:16 -0700

    remove unused stuff

--- remix.config.js
+++ remix.config.js
@@ -1,14 +1,7 @@
-// const { flatRoutes } = require("remix-flat-routes");
-
 /**
  * @type {import('@remix-run/dev').AppConfig}
  */
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
   devServerBroadcastDelay: 500,
-  // ignoredRouteFiles: ["**/.*", "**/*.css", "**/*.test.{js,jsx,ts,tsx}"],
-  // ignoredRouteFiles: ["**/*"],
-  // routes: async (defineRoutes) => {
-  //   return flatRoutes("flatroutes", defineRoutes);
-  // },
 };
