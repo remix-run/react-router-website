4572f6cf3a34c24bee2df83bd84b75e24e3fb85a
Matt Brophy <matt@brophy.org>
2023-03-09 15:48:37 -0500

    Support release-next as a branch name

--- app/modules/gh-docs/index.ts
+++ app/modules/gh-docs/index.ts
@@ -36,6 +36,7 @@ export function getRepoDoc(ref: string, slug: string) {
 function fixupRefName(ref: string) {
   return ref === "dev" ||
     ref === "main" ||
+    ref === "release-next" ||
     ref === "local" ||
     // when we switched to changesets the `v` went away, so we use that as a way
     // to know if we need to add hte `react-router@` prefix for interacting w/
