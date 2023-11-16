e4c8be3e6be67ab9545854de8f3be3d2eed4c5a0
Matt Brophy <matt@brophy.org>
2023-11-15 19:36:34 -0500

    Upgrade v2_dev, v2_normalizeFormMethod, remove devServerBroadcastDelay

--- remix.config.js
+++ remix.config.js
@@ -1,11 +1,13 @@
 /** @type {import('@remix-run/dev').AppConfig} */
 module.exports = {
   cacheDirectory: "./node_modules/.cache/remix",
-  devServerBroadcastDelay: 500,
   future: {
     v2_routeConvention: true,
     v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_dev: true,
   },
   postcss: true,
   tailwind: true,
+  serverModuleFormat: "cjs",
 };
