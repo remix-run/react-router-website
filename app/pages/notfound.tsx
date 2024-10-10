7daf345632d3e6a6b8737634d92d71e530247782
Ryan Florence <rpflorence@gmail.com>
2024-10-10 14:26:00 -0600

    support shorthand versioned URLs
    this lets us link to docs from changelogs and console warnings for a major version and getting them to the latest doc

--- app/pages/notfound.tsx
+++ app/pages/notfound.tsx
@@ -1,8 +1,32 @@
+import { redirect, type LoaderFunctionArgs } from "@remix-run/node";
 import { type MetaFunction } from "@remix-run/react";
 import { Link, isRouteErrorResponse, useRouteError } from "@remix-run/react";
+import * as semver from "semver";
 import { CACHE_CONTROL } from "~/http";
+import { getRepoTags } from "~/modules/gh-docs/.server";
 
-export let loader = async () => {
+/**
+ * This let's us have `v6/*` or `v7/*` URLs in changelogs and console warnings
+ * so we can link people to the latest doc of the version they're using.
+ */
+async function maybeRedirectToDoc(pathname: string) {
+  let [, s, ...rest] = pathname.split("/");
+  let version = s.replace(/^v/, "");
+  let regex = /^\d+(\.\d+)?(\.\d+)?$/;
+  if (regex.test(version)) {
+    let versions = await getRepoTags();
+    let latest = semver.maxSatisfying(versions, `${s}.x`, {
+      includePrerelease: false,
+    });
+    if (latest) {
+      throw redirect(`/en/${latest}/${rest.join("/")}`);
+    }
+  }
+}
+
+export let loader = async ({ request }: LoaderFunctionArgs) => {
+  let url = new URL(request.url);
+  await maybeRedirectToDoc(url.pathname);
   // TODO: use `data` or whatever we end up with in single fetch instead of
   // throwing here
   throw new Response("Not Found", { status: 404 });
