a5d4f241f90a55644fc2bbf23392ce4c96385c6c
Brooks Lybrand <brookslybrand@gmail.com>
2024-10-31 16:36:45 -0500

    Only call  `getRepoDocsReferenceMenu` once (#128)

--- app/components/docs-menu/data.server.ts
+++ app/components/docs-menu/data.server.ts
@@ -11,25 +11,20 @@ export async function loadGuidesMenu(ref: string) {
   return getRepoDocsMenu(ref, "en");
 }
 
-export async function loadReferenceMenu(ref: string, pkg: string) {
+export type Pkg = Awaited<ReturnType<typeof loadPackageData>>["pkgs"][number];
+
+export async function loadPackageData(ref: string, pkg: string) {
   let menu = await getRepoDocsReferenceMenu(ref);
+
   let pkgName = pkg === "react-router" ? pkg : `@react-router/${pkg}`;
   let pkgMenu = menu.find((p) => p.attrs.title === pkgName);
   if (!pkgMenu) throw new Response("Not Found", { status: 404 });
-  return pkgMenu.children;
-}
 
-export type Pkg = {
-  name: string;
-  href: any;
-};
-
-export async function loadPackageNames(ref: string): Promise<Pkg[]> {
-  let menu = await getRepoDocsReferenceMenu(ref);
-  return menu.map((pkg) => {
-    return {
-      name: pkg.attrs.title,
-      href: pkg.attrs.href,
-    };
-  });
+  return {
+    pkgs: menu.map(({ attrs }) => ({
+      name: attrs.title,
+      href: attrs.href,
+    })),
+    menu: pkgMenu.children,
+  };
 }
