6141953a2faa7dd0fb9908258e5c62991f775fa4
Brooks Lybrand <brookslybrand@gmail.com>
2024-11-01 09:27:18 -0500

    Add `shouldRevalidate` to api and guides layout

--- app/pages/guides-layout.tsx
+++ app/pages/guides-layout.tsx
@@ -1,5 +1,5 @@
 import type { LinksFunction, LoaderFunctionArgs } from "@remix-run/node";
-import { Outlet } from "@remix-run/react";
+import { type ShouldRevalidateFunction, Outlet } from "@remix-run/react";
 import classNames from "classnames";
 
 import docsStylesheet from "~/styles/docs.css?url";
@@ -24,6 +24,12 @@ export let loader = async ({ params }: LoaderFunctionArgs) => {
   ]);
 
   return { menu, header };
+};
+
+export const shouldRevalidate: ShouldRevalidateFunction = ({
+  defaultShouldRevalidate,
+}) => {
+  return defaultShouldRevalidate;
 };
 
 export default function DocsLayout() {
