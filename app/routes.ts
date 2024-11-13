fbc78d6c3526c3fbdd0c6d945fa7176232f4cc41
Brooks Lybrand <brookslybrand@gmail.com>
2024-11-13 09:36:43 -0600

    Switch to routes.ts (#130)
    * Upgrade Remix to 2.14.0-pre.0
    
    * Move route definitions to routes.ts
    
    * Remove @remix-run/routes-option-adapter
    
    * Bump to 2.14.0
    
    * Update routes to match update after removed api routes

---
+++ app/routes.ts
@@ -0,0 +1,32 @@
+import { type RouteConfig, route, index } from "@remix-run/route-config";
+
+export const routes: RouteConfig = [
+  index("pages/splash.tsx"),
+  route("/brand", "pages/brand.tsx"),
+  route("/healthcheck", "pages/healthcheck.tsx"),
+  route("/color-scheme", "actions/color-scheme.ts"),
+
+  route("/:ref?", "pages/docs-layout.tsx", { id: "docs" }, [
+    index("pages/docs-index.tsx"),
+    route("home", "pages/doc.tsx", { id: "home" }),
+    route("*", "pages/doc.tsx"),
+  ]),
+
+  route("/*", "pages/notfound.tsx"),
+
+  // short version URLs for changelogs and stuff
+  route("/v6/*", "pages/redirect-v6-doc.tsx"),
+  route("/v7/*", "pages/redirect-v7-doc.tsx"),
+
+  // v6 URLs before the api reference docs
+  route("/en/:ref", "pages/docs-layout.tsx", { id: "v6-docs" }, [
+    index("pages/docs-home.tsx", {
+      id: "v6-index",
+    }),
+    route("*", "pages/doc.tsx", { id: "v6-guide" }),
+  ]),
+];
+
+if (process.env.NODE_ENV === "development") {
+  routes.push(route("/__playground", "components/_playground/playground.tsx"));
+}
