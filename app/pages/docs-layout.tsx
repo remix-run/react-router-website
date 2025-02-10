fff80e01f1fb885f5a0c0e1e37f144e22f76d3c3
Brooks Lybrand <brookslybrand@gmail.com>
2025-02-10 16:40:02 -0600

    Refactor routing and redirects for v6 documentation

--- app/pages/docs-layout.tsx
+++ app/pages/docs-layout.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from "react-router";
+import { Outlet, redirect } from "react-router";
 import classNames from "classnames";
 
 import { Header } from "~/components/docs-header/docs-header";
@@ -15,7 +15,18 @@ import { useCodeBlockCopyButton } from "~/ui/utils";
 
 import docsCss from "~/styles/docs.css?url";
 
-export let loader = async ({ params }: Route.LoaderArgs) => {
+export let loader = async ({ request, params }: Route.LoaderArgs) => {
+  let url = new URL(request.url);
+  if (!url.pathname.endsWith("/")) {
+    url.pathname += "/";
+  }
+
+  // the /:ref param should only be used for v6 docs
+  // if ref is not a v6 ref, redirect to the /home of that ref
+  if (params.ref && !url.pathname.match(/^\/?(6)/)) {
+    throw redirect(url.pathname + "home");
+  }
+
   let splat = params["*"];
   let firstSegment = splat?.split("/")[0];
   let refParam = params.ref
@@ -37,10 +48,16 @@ export let loader = async ({ params }: Route.LoaderArgs) => {
 };
 
 export default function DocsLayout({ loaderData }: Route.ComponentProps) {
-  const { menu } = loaderData;
+  const { menu, header } = loaderData;
 
   let docsContainer = useRef<HTMLDivElement>(null);
   useCodeBlockCopyButton(docsContainer);
+
+  const changelogHref = header.hasAPIDocs
+    ? header.refParam
+      ? `/${header.refParam}/changelog`
+      : "/changelog"
+    : undefined;
 
   return (
     <>
@@ -49,13 +66,13 @@ export default function DocsLayout({ loaderData }: Route.ComponentProps) {
         <div className="sticky top-0 z-20">
           <Header />
           <NavMenuMobile>
-            <Menu menu={menu} />
+            <Menu menu={menu} changelogHref={changelogHref} />
           </NavMenuMobile>
         </div>
 
         <div className="block lg:flex">
           <NavMenuDesktop>
-            <Menu menu={menu} />
+            <Menu menu={menu} changelogHref={changelogHref} />
           </NavMenuDesktop>
           <div
             ref={docsContainer}
--- app/pages/redirect-major-version.tsx
+++ app/pages/redirect-major-version.tsx
@@ -1,7 +1,7 @@
 import { redirect } from "react-router";
 import { getRepoTags } from "~/modules/gh-docs/.server";
 import * as semver from "semver";
-import type { Route } from "./+types/redirect-v6-doc";
+import type { Route } from "./+types/redirect-major-version";
 
 export async function loader({ request }: Route.LoaderArgs) {
   let url = new URL(request.url);
@@ -11,8 +11,9 @@ export async function loader({ request }: Route.LoaderArgs) {
   let latest = semver.maxSatisfying(versions, `${s}.x`, {
     includePrerelease: false,
   });
+
   if (latest) {
-    return redirect(`/en/${latest}/${rest.join("/")}${url.search}`);
+    return redirect(`/${latest}/${rest.join("/")}${url.search}`);
   }
 
   return new Response("Not Found", { status: 404 });
