3c5186a3b6255c2892f0485ff980fd0bd31b6639
Jacob Ebey <jacob.ebey@live.com>
2025-02-13 10:20:26 -0800

    feat: enable turbo-v3

--- react-router.config.ts
+++ react-router.config.ts
@@ -2,6 +2,7 @@ import type { Config } from "@react-router/dev/config";
 
 export default {
   future: {
+    turboV3: true,
     unstable_optimizeDeps: true,
     unstable_splitRouteModules: "enforce",
   },
