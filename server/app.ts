3e026b65ade81686c591f96c2f3c0d174ed70b57
Brooks Lybrand <brookslybrand@gmail.com>
2025-04-22 10:05:45 -0500

    Remove @ts-expect-error comment

--- server/app.ts
+++ server/app.ts
@@ -5,7 +5,6 @@ export const app = express();
 
 app.use(
   createRequestHandler({
-    // @ts-expect-error - virtual module provided by React Router at build time
     build: () => import("virtual:react-router/server-build"),
   }),
 );
