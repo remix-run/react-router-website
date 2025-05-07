da4b22d749281d94ac9a921b8b372b2d585bf398
Brooks Lybrand <brookslybrand@gmail.com>
2025-05-07 10:27:11 -0500

    Bump to 7.6.0-pre.0

--- react-router.config.ts
+++ react-router.config.ts
@@ -1,11 +1,5 @@
 import type { Config } from "@react-router/dev/config";
 
-declare module "react-router" {
-  interface Future {
-    unstable_middleware: true;
-  }
-}
-
 export default {
   future: {
     unstable_optimizeDeps: true,
