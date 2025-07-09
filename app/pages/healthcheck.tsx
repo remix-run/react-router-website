fac02a7143d8631dace559c37a89bcd84669a408
Michael Jackson <michael@jackson.us>
2025-07-09 10:25:03 -0700

    Return OK directly from healthcheck loader

--- app/pages/healthcheck.tsx
+++ app/pages/healthcheck.tsx
@@ -2,19 +2,19 @@
 import type { LoaderFunction } from "react-router";
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const host =
-    request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
+  // const host =
+  //   request.headers.get("X-Forwarded-Host") ?? request.headers.get("host");
 
-  try {
-    const url = new URL("/", `http://${host}`);
-    await Promise.all([
-      fetch(url.toString(), { method: "HEAD" }).then((r) => {
-        if (!r.ok) return Promise.reject(r);
-      }),
-    ]);
+  // try {
+  //   const url = new URL("/", `http://${host}`);
+  //   await Promise.all([
+  //     fetch(url.toString(), { method: "HEAD" }).then((r) => {
+  //       if (!r.ok) return Promise.reject(r);
+  //     }),
+  //   ]);
     return new Response("OK");
-  } catch (error: unknown) {
-    console.log("healthcheck ❌", { error });
-    return new Response("ERROR", { status: 500 });
-  }
+  // } catch (error: unknown) {
+  //   console.log("healthcheck ❌", { error });
+  //   return new Response("ERROR", { status: 500 });
+  // }
 };
