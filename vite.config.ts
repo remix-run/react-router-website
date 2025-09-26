f4a1c8454b554c9c19cc6dc27f10dbd72d3995b8
Brooks Lybrand <brookslybrand@gmail.com>
2025-09-26 12:17:22 -0500

    Revert knip and analyzer

--- vite.config.ts
+++ vite.config.ts
@@ -1,41 +1,17 @@
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { visualizer } from "rollup-plugin-visualizer";
-import { analyzer } from "vite-bundle-analyzer";
 
-export default defineConfig(({ isSsrBuild }) => {
-  const plugins = [reactRouter(), tsconfigPaths()];
-
-  // Add bundle analyzer plugins when ANALYZE env var is set
-  if (process.env.ANALYZE && !isSsrBuild) {
-    plugins.push(
-      visualizer({
-        filename: "build/client/stats.html",
-        open: false,
-        gzipSize: true,
-        brotliSize: true,
-        template: "treemap",
-      }),
-      analyzer({
-        analyzerMode: "static",
-        fileName: "bundle-report.html",
-        openAnalyzer: false,
-      }),
-    );
-  }
-
-  return {
-    build: {
-      rollupOptions: isSsrBuild
-        ? {
-            input: "./server/app.ts",
-          }
-        : undefined,
-    },
-    ssr: {
-      noExternal: ["@docsearch/react"],
-    },
-    plugins,
-  };
-});
+export default defineConfig(({ isSsrBuild }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: "./server/app.ts",
+        }
+      : undefined,
+  },
+  ssr: {
+    noExternal: ["@docsearch/react"],
+  },
+  plugins: [reactRouter(), tsconfigPaths()],
+}));
