3f9063d2c209a09144e5b47cfa049328e1f09e5e
Brooks Lybrand <brookslybrand@gmail.com>
2025-12-01 10:10:39 -0600

    Simplify vite config

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,7 @@ import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig(({ isSsrBuild }) => ({
-  build: {
-    rollupOptions: isSsrBuild
-      ? {
-          input: "./server/app.ts",
-        }
-      : undefined,
-  },
+export default defineConfig(() => ({
   ssr: {
     noExternal: ["@docsearch/react"],
   },
