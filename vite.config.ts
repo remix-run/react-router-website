b718802e9a6a56996d33c9c224f5028896cf0e22
Brooks Lybrand <brookslybrand@gmail.com>
2025-12-01 10:18:46 -0600

    Fix config for server build `rollupOptions`

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,15 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig(() => ({
+  environments: {
+    ssr: {
+      build: {
+        rollupOptions: {
+          input: "./server/app.ts",
+        },
+      },
+    },
+  },
   ssr: {
     noExternal: ["@docsearch/react"],
   },
